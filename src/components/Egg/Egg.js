fdb8c6cb28edc38118d8146f62017594fd8b5021
Paul <plee032@uottawa.ca>
2018-10-16 21:45:04 -0400

    codemonkey

--- src/components/Egg/Egg.js
+++ src/components/Egg/Egg.js
@@ -4,6 +4,7 @@ import { Modal, Image } from "semantic-ui-react";
 import Konami from "react-konami-code";
 import Dev from "../../images/slaveDev.jpg";
 import Nits from "../../images/nits.png";
+import Arsani from "../../images/arsani.png";
 
 //use KeyCodes for entering codes
 
@@ -33,10 +34,25 @@ const nitsCode = [
   13
 ];
 
+const arsaniCode = [
+  //codemonkey
+  67,
+  79,
+  68,
+  69,
+  77,
+  79,
+  78,
+  75,
+  69,
+  89
+];
+
 export default class Egg extends Component {
   state = {
     open: false,
-    nitsAnimate: "off"
+    nitsAnimate: "off",
+    arsaniAnimate: "off"
   };
 
   onClose = () => this.setState({ open: false });
@@ -50,12 +66,23 @@ export default class Egg extends Component {
       "https://sound.peal.io/ps/audios/000/000/366/original/i'm-the-man.mp3"
     );
     audio.play();
-    var css = this.state.nitsAnimate === "off" ? "animatedIn" : "animatedIn";
-    this.setState({ nitsAnimate: css });
+    this.setState({ nitsAnimate: "animatedIn-Nits" });
   };
 
   nitsClose = () => {
     this.setState({ nitsAnimate: "off" });
+  };
+
+  arsani = () => {
+    var audio = new Audio(
+      "https://sound.peal.io/ps/audios/000/001/509/original/youtube.mp3?1487857996"
+    );
+    audio.play();
+    this.setState({ arsaniAnimate: "animatedIn-Arsani" });
+  };
+
+  arsaniClose = () => {
+    this.setState({ arsaniAnimate: "off" });
   };
 
   render() {
@@ -76,7 +103,20 @@ export default class Egg extends Component {
           onTimeout={this.nitsClose}
           className="nitskonami"
         >
-          <Image src={Nits} className={this.state.nitsAnimate + " nits"} />
+          <Image src={Nits} className={this.state.nitsAnimate + " " + "nits"} />
+        </Konami>
+
+        <Konami
+          action={this.arsani}
+          code={arsaniCode}
+          timeout={16000}
+          onTimeout={this.arsaniClose}
+          className="arsaniKonami"
+        >
+          <Image
+            src={Arsani}
+            className={this.state.arsaniAnimate + " " + "arsani"}
+          />
         </Konami>
       </div>
     );
