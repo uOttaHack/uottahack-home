9c12420f64c834e44406f450a5f031da4867f7d8
Taj Loodu <tajloodu@gmail.com>
2022-08-11 17:13:11 -0400

    Changed to semantic-ui grid and added new sponsor images

--- src/components/Sponsor/Sponsor.js
+++ src/components/Sponsor/Sponsor.js
@@ -16,26 +16,28 @@ const images = importAll(
 class Sponsor extends Component {
   render() {
     return (
-      <div className="Sponsor">
-        <div className="sponsor-row">
-          <img src={images["canada.png"]} style={{ width: "500px" }} />
+      <div className="ui centered grid">
+        <div className="row">
+          <img src={images["innovapost.png"]} style={{ width: "400px" }} />
+          <img src={images["nokia.png"]} style={{ width: "200px" }} />
+          <img src={images["ciena.png"]} style={{ width: "200px" }} />
         </div>
-        <div className="sponsor-row">
+        <div className="row">
           <img src={images["klipfolio.png"]} style={{ width: "200px" }} />
-          <img src={images["surveymonkey.png"]} style={{ width: "320px" }} />
+          <img src={images["surveymonkey.png"]} style={{ width: "300px" }} />
           <img src={images["marchnetworks.png"]} style={{ width: "200px" }} />
         </div>
-        <div className="sponsor-row">
+        <div className="row">
           <img src={images["deloitte.png"]} style={{ width: "200px" }} />
           <img src={images["morganstanley.png"]} style={{ width: "275px" }} />
           <img src={images["youi.png"]} style={{ width: "100px" }} />
         </div>
-        <div className="sponsor-row">
+        <div className="row">
           <img src={images["sunlife.png"]} style={{ width: "200px" }} />
           <img src={images["agilebits.png"]} style={{ width: "150px" }} />
           <img src={images["accenture.png"]} style={{ width: "170px" }} />
         </div>
-        <div className="sponsor-row">
+        <div className="row">
           <img src={images["bitesite.png"]} style={{ width: "200px" }} />
           <img src={images["kanatanorthbia.png"]} style={{ width: "150px" }} />
           <img src={images["phreesia.png"]} style={{ width: "200px" }} />
