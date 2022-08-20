3df4e91a8957dee6a22dd5bc4fa211dc84e1224d
Taj Loodu <tajloodu@gmail.com>
2022-08-20 16:11:55 -0400

    Changed card format

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -8,22 +8,10 @@ const items1 = [
     meta: "Flagship Hackathon",
     description:
       "Our flagship event, the university's official MLH hackathon with over 600+ hackers and 20+ sponsors!"
-  },
-  {
-    header: "init uOttaHack 5",
-    meta: "Intro Hackathon",
-    description:
-      "The catalyst to our hacking season, init is the university's official MLH Global Hack Week hackathon."
   }
 ];
 
 const items2 = [
-  {
-    header: "Workshops",
-    meta: "Community",
-    description:
-      "Various workshops held on campus focused on helping students learn skills outside the classroom."
-  },
   {
     header: "Tech Intern Panel",
     meta: "Community",
@@ -32,12 +20,54 @@ const items2 = [
   }
 ];
 
+const items3 = [
+  {
+    header: "init uOttaHack 5",
+    meta: "Intro Hackathon",
+    description:
+      "The catalyst to our hacking season, init is the university's official MLH Global Hack Week hackathon."
+  }
+];
+
+const items4 = [
+  {
+    header: "Workshops",
+    meta: "Community",
+    description:
+      "Various workshops held on campus focused on helping students learn skills outside the classroom."
+  }
+];
+
 class Event extends Component {
   render() {
     return (
       <div className="Event">
-        <Card.Group centered items={items1} />
-        <Card.Group centered items={items2} />
+        <div className="ui centered grid">
+          <div className="row" id="cardRow">
+            <Card
+              header="uOttawaHack 5"
+              meta="Flagship Hackathon"
+              description="Our flagship event, the university's official MLH hackathon with over 600+ hackers and 20+ sponsors!"
+            />
+            <Card
+              header="Tech Intern Panel"
+              meta="Community"
+              description="A panel of several students who share their experiences working internships at top tech companies."
+            />
+          </div>
+          <div className="row" id="cardRow">
+            <Card
+              header="init uOttaHack 5"
+              meta="Intro Hackathon"
+              description="The catalyst to our hacking season, init is the university's official MLH Global Hack Week hackathon."
+            />
+            <Card
+              header="Workshops"
+              meta="Community"
+              description="Various workshops held on campus focused on helping students learn skills outside the classroom."
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Organizer/Organizer.js
+++ src/components/Organizer/Organizer.js
@@ -2,45 +2,6 @@ import React, { Component } from "react";
 import "./Organizer.css";
 import organizer2022 from "./organizers2022.json";
 import { Card, Button } from "semantic-ui-react";
-import "@mobiscroll/react-lite/dist/css/mobiscroll.min.css";
-
-const chair18 = [
-  {
-    imgsrc: "../../images/paul-lee.png",
-    header: "Paul Lee",
-    meta: "Co-chair",
-    website: "",
-    linkedin: "",
-    github: ""
-  },
-  {
-    imgsrc: "../../images/matt-langlois.png",
-    header: "Matt Langlois",
-    meta: "Co-chair",
-    website: "",
-    linkedin: "",
-    github: ""
-  }
-];
-
-const chair19 = [
-  {
-    imgsrc: "../../images/paul-lee.png",
-    header: "Paul Lee",
-    meta: "Co-Director",
-    website: "",
-    linkedin: "",
-    github: ""
-  },
-  {
-    imgsrc: "../../images/hamza-basrai.png",
-    header: "Hamza Basrai",
-    meta: "Co-Director",
-    website: "",
-    linkedin: "",
-    github: ""
-  }
-];
 
 const chair22 = [
   {
@@ -67,8 +28,8 @@ class Organizer extends Component {
       <div className="Organizer">
         <div className="Organizer-Group">
           <h3>2022-2023</h3>
-          <Card.Group centered items={chair22} />
-          <Card.Group centered items={organizer2022} />
+          <Card.Group centered items={chair22} id="cards" />
+          <Card.Group centered items={organizer2022} id="cards" />
         </div>
       </div>
     );
