9894bfb4dfb678d2e83cc2b1430d819589d3ff65
Eric Dam <edam3268@gmail.com>
2022-11-30 02:37:33 -0500

    Removed z index on front

--- src/components/Front/Front.js
+++ src/components/Front/Front.js
@@ -14,7 +14,7 @@ const front = () => {
         </div>
         <div class="HeroText">
           <h1>The University of Ottawa's Hackathon Community</h1>
-          <div className="logobutton" z-index="99">
+          <div className="logobutton">
             <a href="https://2023.uottahack.ca/" target="_blank">
               <button className="btn">Visit 2023 →</button>
             </a>
